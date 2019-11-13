aa12a62e69f5a79e5437a13c2ad76f150b0e1a29
Mauro Reis Vieira <mauroreisvieira@gmail.com>
2019-11-13 18:10:05 +0000

    feat: add colors light and dark.

--- src/build.js
+++ src/build.js
@@ -3,6 +3,8 @@ const path = require("path")
 const fs = require("fs")
 const rimraf = require("rimraf")
 
+const {light, dark} = require("./settings/")
+
 rimraf("colors", () => {
     fs.mkdir("colors", () => {
         fs.writeFileSync(`${}.js`,
---
+++ src/settings/index.js
@@ -0,0 +1,8 @@
+/*eslint-disable no-undef */
+const light = require("./light")
+const dark = require("./dark")
+
+module.exports = {
+    light: light,
+    dark: dark
+}
