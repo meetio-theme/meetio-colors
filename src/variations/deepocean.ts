06b1a43e66c9e24b433050f1b4bc1cb6f59d609f
TheSecEng <zachary.schulze@gmail.com>
2021-04-07 15:51:07 -0700

    feat: added new color variation
    added Deep Ocean

---
+++ src/variations/deepocean.ts
@@ -0,0 +1,28 @@
+import { IColors } from './../interfaces';
+
+export const deepocean: IColors = {
+    accent: '#80CBC4',
+    foreground: '#DEDFE4',
+    background: '##011627',
+    cursor: '#FFCC00',
+    comments: '#697098',
+    deprecated: '#FFC777A1',
+    diffAdded: '#9CCC65',
+    diffDeleted: '#EF5350',
+    diffModified: '#E2B93D',
+    invalid: '#D3423E66',
+    base: {
+        black: '#000000',
+        blue: "#82AAFF",
+        brown: "#996667",
+        cyan: "#7FDBCA",
+        green: "#C3E88D",
+        orange: "#F78C6C",
+        pink: "#FF5899",
+        purple: "#C2AFFF",
+        red: "#D95757",
+        violet: '#BB80B3',
+        white: '#FFFFFF',
+        yellow: "#ECF991",
+    },
+};
